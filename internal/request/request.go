1c7abd16feced9a0561abb24e975f155c4f4c5ae
nom-d <75618727+n0m-d@users.noreply.github.com>
2026-06-24 18:11:50 +0545

    Add body parsing to Request and corresponding tests

--- internal/request/request.go
+++ internal/request/request.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"io"
+	"strconv"
 
 	"example.com/HProtocol/internal/headers"
 )
@@ -13,12 +14,14 @@ const (
 	StateDone    parserState = "done"
 	StateError   parserState = "error"
 	StateHeaders parserState = "headers"
+	StateBody    parserState = "body"
 )
 
 type Request struct {
 	RequestLine RequestLine
 	Headers     headers.Headers
 	state       parserState
+	Body        string
 }
 
 type RequestLine struct {
@@ -44,10 +47,20 @@ func (r *Request) error() bool {
 	return r.state == StateError
 }
 
+func parseHeaderInt(h headers.Headers, name string, defaultVal int) int {
+	val := h.Get(name)
+	num, err := strconv.Atoi(val)
+	if err != nil {
+		return defaultVal
+	}
+	return num
+}
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
 	request := &Request{
 		state:   StateInit,
 		Headers: headers.Headers{},
+		Body:    "",
 	}
 	buff := make([]byte, 1024)
 	buffLen := 0
@@ -84,11 +97,15 @@ func (r *Request) Parse(data []byte) (int, error) {
 	read := 0
 outer:
 	for {
+		currentData := data[read:]
+		if len(currentData) == 0 {
+			break outer
+		}
 		switch r.state {
 		case StateError:
 			return 0, ERROR_REQUEST_IN_ERROR_STATE
 		case StateInit:
-			rl, n, err := parseRequestLine(data[read:])
+			rl, n, err := parseRequestLine(currentData)
 			if err != nil {
 				return 0, nil
 			}
@@ -105,8 +122,9 @@ outer:
 			break outer
 
 		case StateHeaders:
-			n, done, err := r.Headers.Parse(data[read:])
+			n, done, err := r.Headers.Parse(currentData)
 			if err != nil {
+				r.state = StateError
 				return 0, err
 			}
 
@@ -116,9 +134,24 @@ outer:
 
 			read += n
 			if done {
+				r.state = StateBody
+			}
+
+		case StateBody:
+			cLen := parseHeaderInt(r.Headers, "content-length", 0)
+			if cLen == 0 {
+				r.state = StateDone
+			}
+
+			remaining := min(cLen-len(r.Body), len(currentData))
+			r.Body += string(currentData[:remaining])
+			read += remaining
+
+			if len(r.Body) == cLen {
 				r.state = StateDone
 			}
 		}
+
 	}
 	return read, nil
 }
